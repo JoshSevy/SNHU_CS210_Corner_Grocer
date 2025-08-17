f17bbc9f2185b5a790d0f6bef87c952cba2431db
Joshua Sevy <joshuasevy@outlook.com>
2025-08-16 20:18:57 -0600

    Update main after testing

--- src/Menu.h
+++ src/Menu.h
@@ -6,12 +6,19 @@
 #ifndef MENU_H
 #define MENU_H
 
-
+class GroceryTracker;
 
 class Menu {
+public:
+    explicit Menu(GroceryTracker &tracker); // Constructor that takes a reference to a GroceryTracker object
+    void run() const; // Runs the menu loop until the user chooses to exit
 
+private:
+    GroceryTracker& tracker;
+    void doSearch() const; // Searches for an item in the frequency map
+    void doPrintAll() const; // Prints all items and their counts
+    void doHistogram() const; // Prints a histogram of the frequency map
+    int promptChoice() const; // Prompts the user for a menu choice
 };
-
-
 
 #endif //MENU_H
